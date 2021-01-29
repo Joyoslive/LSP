64dd93342c62ff8f9921af1a485f1d55d9baab3e
Jumpscaren <58552272+Jumpscaren@users.noreply.github.com>
2021-01-29 17:17:18 +0100

    Add Get and Set functions
    Add positonMatrix, scaleMatrix and rotationMatrix

--- Season_Shift/Season_Shift/Transform.cpp
+++ Season_Shift/Season_Shift/Transform.cpp
@@ -1,8 +1,19 @@
 #include "Transform.h"
 
+Transform::Transform(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 scale, DirectX::XMFLOAT3 rotation)
+{
+	m_position = position;
+	m_scale = scale;
+	m_rotation = rotation;
+	setMatrixs();
+}
+
 Transform::Transform()
 {
-
+	m_position = DirectX::XMFLOAT3(0, 0, 0);
+	m_scale = DirectX::XMFLOAT3(1, 1, 1);
+	m_rotation = DirectX::XMFLOAT3(0, 0, 0);
+	setMatrixs();
 }
 
 Transform::~Transform()
@@ -10,7 +21,64 @@ Transform::~Transform()
 	
 }
 
-DirectX::XMMATRIX Transform::getWorldMatrix()
+void Transform::setMatrixs()
 {
-	return DirectX::XMLoadFloat4x4(&m_worldMatrix);
+	setPositionMatrix();
+	setScaleMatrix();
+	setRotationMatrix();
 }
+
+void Transform::setPositionMatrix()
+{
+	m_positionMatrix = DirectX::XMMatrixTranslation(m_position.x, m_position.y, m_position.z);
+	setPositionMatrix();
+}
+
+void Transform::setScaleMatrix()
+{
+	m_scaleMatrix = DirectX::XMMatrixScaling(m_scale.x, m_scale.y, m_scale.z);
+	setScaleMatrix();
+}
+
+void Transform::setRotationMatrix()
+{
+	m_rotationMatrix = DirectX::XMMatrixRotationRollPitchYaw(m_rotation.x, m_rotation.y, m_rotation.z);
+	setRotationMatrix();
+}
+
+void Transform::setPosition(DirectX::XMFLOAT3 position)
+{
+	m_position = position;
+}
+
+void Transform::setScale(DirectX::XMFLOAT3 scale)
+{
+	m_scale = scale;
+}
+
+void Transform::setRotation(DirectX::XMFLOAT3 rotation)
+{
+	m_rotation = rotation;
+}
+
+DirectX::XMFLOAT3 Transform::getPosition() const
+{
+	return m_position;
+}
+
+DirectX::XMFLOAT3 Transform::getScale() const
+{
+	return m_scale;
+}
+
+DirectX::XMFLOAT3 Transform::getRotation() const
+{
+	return m_rotation;
+}
+
+DirectX::XMMATRIX Transform::getWorldMatrix() const
+{
+	DirectX::XMMATRIX worldMatrix = m_scaleMatrix * m_rotationMatrix * m_positionMatrix;
+	return worldMatrix;
+}
+
--- Season_Shift/Season_Shift/Transform.h
+++ Season_Shift/Season_Shift/Transform.h
@@ -8,14 +8,28 @@ private:
 	DirectX::XMFLOAT3 m_scale;
 	DirectX::XMFLOAT3 m_rotation;
 
-	DirectX::XMFLOAT4X4 m_worldMatrix;
+	//DirectX::XMFLOAT4X4 m_worldMatrix;
+	DirectX::XMMATRIX m_rotationMatrix;
+	DirectX::XMMATRIX m_positionMatrix;
+	DirectX::XMMATRIX m_scaleMatrix;
+	
+private:
+	void setMatrixs();
+	void setPositionMatrix();
+	void setScaleMatrix();
+	void setRotationMatrix();
 public:
+	Transform(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 scale, DirectX::XMFLOAT3 rotation);
 	Transform();
+
 	~Transform();
 
-	DirectX::XMFLOAT3 getPosition();
-	DirectX::XMFLOAT3 getScale();
-	DirectX::XMFLOAT3 getRotation();
-	DirectX::XMMATRIX getWorldMatrix();
+	void setPosition(DirectX::XMFLOAT3 position);
+	void setScale(DirectX::XMFLOAT3 scale);
+	void setRotation(DirectX::XMFLOAT3 rotation);
+	DirectX::XMFLOAT3 getPosition() const;
+	DirectX::XMFLOAT3 getScale() const;
+	DirectX::XMFLOAT3 getRotation() const;
+	DirectX::XMMATRIX getWorldMatrix() const;
 };
 
