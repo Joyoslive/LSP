8666e794b43968a597f0447330f454baabaa8e5c
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-01 13:29:04 +0100

    Add simpleMath to rigidbody

--- Season_Shift/Season_Shift/RigidBodyComponent.h
+++ Season_Shift/Season_Shift/RigidBodyComponent.h
@@ -1,19 +1,22 @@
 #pragma once
 #include "Component.h"
+#include <DirectXMath.h>
+#include <d3d11.h>
+#include <SimpleMath.h>
 
 class RigidBodyComponent : public Component
 {
 private:
 	float m_mass = 1;
 	float m_gravity = 10;
-	float m_force[3] = { 0,0,0 };
-	float m_velocity[3] = { 0,0,0 };
-	float m_acceleration[3] = { 0,0,0 };
+	DirectX::SimpleMath::Vector3 m_force;
+	DirectX::SimpleMath::Vector3 m_velocity;
+	DirectX::SimpleMath::Vector3 m_acceleration;
 	
 
 
 public:
-	RigidBodyComponent();
+	RigidBodyComponent(float mass = 1);
 	void setMass(float mass);
 	float getMass() const;
 
