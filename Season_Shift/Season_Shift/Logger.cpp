aa40cf0f3cda9bbd0bd012a45b67e2fb9cc07af2
Joyos <joyoslive@gmail.com>
2021-02-01 13:44:17 +0100

    Add functionality to add logs to the buffered logs
    Add the option to add a log to the list of logs to be printed when dumpLogs is called. (aside: dumpLogs is not yet implemented)

--- Season_Shift/Season_Shift/Logger.cpp
+++ Season_Shift/Season_Shift/Logger.cpp
@@ -1,5 +1,9 @@
 #include "Logger.h"
 #include <chrono>
+#include <time.h>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
 #include <codecvt>
 #include <locale>
 #include <Windows.h>
@@ -27,12 +31,22 @@ Logger& Logger::getLogger()
 
 void Logger::addLog(std::string log, std::string filePath)
 {
+	auto timeStamp = std::chrono::system_clock::now();
+	time_t timetTime = std::chrono::system_clock::to_time_t(timeStamp);
+	std::stringstream ss;
+	tm date;
+	localtime_s(&date, &timetTime);
+	ss << std::put_time(&date, "%F %T");
+	Log newLog = {
+		log, ss.str(), filePath
+	};
+	m_bufferedLogs.emplace_back(newLog);
 }
 
 void Logger::debugLog(std::string log)
 {
 #ifdef UNICODE
-	size_t reqSize = MultiByteToWideChar(CP_UTF8, 0, log.c_str(), -1, nullptr, 0);
+	auto reqSize = MultiByteToWideChar(CP_UTF8, 0, log.c_str(), -1, nullptr, 0);
 	auto wLog = new wchar_t[reqSize];
 	MultiByteToWideChar(CP_UTF8, 0, log.c_str(), -1, wLog, reqSize);
 	OutputDebugStringW(wLog);
