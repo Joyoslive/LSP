c99166c25a05675631c87be8dfc05ff00f8594d4
Jumpscaren <58552272+Jumpscaren@users.noreply.github.com>
2021-02-01 16:04:28 +0100

    Add Scene

---
+++ Season_Shift/Season_Shift/Scene.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include "GameObject.h"
+
+class Scene
+{
+private:
+	std::vector<Ref<GameObject>> m_sceneGameObjects;
+private:
+	void addGameObject(Ref<GameObject> gameObject);
+	void removeGameObject(Ref<GameObject> gameObject);
+public:
+	Scene();
+	~Scene();
+	void setUpScene();
+	void start();
+	void update();
+	Ref<GameObject> createGameObject(std::string gameObjectName = "", DirectX::SimpleMath::Vector3 position = DirectX::SimpleMath::Vector3(0, 0, 0)
+		, DirectX::SimpleMath::Vector3 scale = DirectX::SimpleMath::Vector3(1, 1, 1)
+		, DirectX::SimpleMath::Vector3 rotation = DirectX::SimpleMath::Vector3(0, 0, 0));
+	void destroyGameObject(Ref<GameObject> destroyGameObject);
+};
+
