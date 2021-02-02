0874d5724e60d1424f2cbbfdd32ea5789c410c85
Joyos <joyoslive@gmail.com>
2021-02-02 14:45:44 +0100

    Add shader and texture binding

--- Season_Shift/Season_Shift/Material.cpp
+++ Season_Shift/Season_Shift/Material.cpp
@@ -5,13 +5,25 @@ Material::Material(MapPaths maps)
 	m_vertexShader = nullptr;
 	m_pixelShader = nullptr;
 	m_textures = {nullptr, nullptr, nullptr, nullptr};
+
+	// TODO: Set textures
 }
 
 Material::~Material()
-{}
+{
+	
+}
 
 void Material::bindShader(std::shared_ptr<DXDevice> device)
-{}
+{
+	device->bindShader(m_vertexShader, DXShader::Type::VS);
+	device->bindShader(m_pixelShader, DXShader::Type::PS);
+}
 
 void Material::bindTextures(std::shared_ptr<DXDevice> device)
-{}
+{
+	device->bindShaderTexture(DXShader::Type::PS, 0, m_textures.ambient);
+	device->bindShaderTexture(DXShader::Type::PS, 1, m_textures.diffuse);
+	device->bindShaderTexture(DXShader::Type::PS, 2, m_textures.specular);
+	device->bindShaderTexture(DXShader::Type::PS, 3, m_textures.normal);
+}
