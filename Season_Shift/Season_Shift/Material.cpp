a7dda5b58ac4da4790988be77492a7425db1b882
Joyos <joyoslive@gmail.com>
2021-02-02 14:26:00 +0100

    Add basic structure of Material

--- Season_Shift/Season_Shift/Material.cpp
+++ Season_Shift/Season_Shift/Material.cpp
@@ -1,1 +1,17 @@
 #include "Material.h"
+
+Material::Material(MapPaths maps)
+{
+	m_vertexShader = nullptr;
+	m_pixelShader = nullptr;
+	m_textures = {nullptr, nullptr, nullptr, nullptr};
+}
+
+Material::~Material()
+{}
+
+void Material::bindShader(std::shared_ptr<DXDevice> device)
+{}
+
+void Material::bindTextures(std::shared_ptr<DXDevice> device)
+{}
--- Season_Shift/Season_Shift/Material.h
+++ Season_Shift/Season_Shift/Material.h
@@ -1,12 +1,36 @@
 #pragma once
+
+#include <string>
+#include "Graphics/DX/DXTexture.h"
+#include "Graphics/DX/DXDevice.h"
+
 class Material
 {
 private:
-
-
-
+	struct PhongMaps
+	{
+		std::shared_ptr<DXTexture> ambient;
+		std::shared_ptr<DXTexture> diffuse;
+		std::shared_ptr<DXTexture> specular;
+		std::shared_ptr<DXTexture> normal;
+	};
+private:
+	std::shared_ptr<DXShader> m_vertexShader,  m_pixelShader;
+	PhongMaps m_textures;
 
 public:
+	struct MapPaths
+	{
+		std::string ambient;
+		std::string diffuse;
+		std::string specular;
+		std::string normal;
+	};
+public:
+	Material(MapPaths maps);
+	~Material();
 
+	void bindShader(std::shared_ptr<DXDevice> device);
+	void bindTextures(std::shared_ptr<DXDevice> device);
 };
 
