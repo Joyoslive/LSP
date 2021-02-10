9ee523fc6cb19a312d0e68d623334c0a370a7323
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-10 11:42:49 +0100

    Setup scene2 and Add models

--- Season_Shift/Season_Shift/Scenes/Scene2.cpp
+++ Season_Shift/Season_Shift/Scenes/Scene2.cpp
@@ -17,10 +17,21 @@ Scene2::~Scene2()
 
 void Scene2::setUpScene()
 {
-	Ref<GameObject> go5 = createGameObject("player", Vector3(0, 0, 0), Vector3(1.f, 1.f, 1.f));
-	go5->AddComponent(m_graphics->getResourceDevice()->createModel("Models/Cube/", "Cube.obj", GfxShader::DEFAULT));
-	go5->AddComponent(std::make_shared<CameraComponent>());
+	Ref<GameObject> player = createGameObject("player", Vector3(0, 10, 0), Vector3(0.2f, 0.2f, 0.2f));
+	//player->AddComponent(m_graphics->getResourceDevice()->createModel("Models/capsule/", "capsule.obj", GfxShader::DEFAULT));
+	player->AddComponent(m_graphics->getResourceDevice()->createModel("Models/sphere/", "sphere.obj", GfxShader::DEFAULT));
+	player->AddComponent(std::make_shared<CameraComponent>());
+	player->AddComponent(std::make_shared<RigidBody>());
+	player->AddComponent(std::make_shared<Player>());
+	//player->AddComponent(std::make_shared<CapsuleCollider>(1, 4));
+	player->AddComponent(std::make_shared<SphereCollider>(1));
+	
 
-	Ref<GameObject> go1 = createGameObject("object", Vector3(0, 0, 20.0f));
+
+	Ref<GameObject> brickCube = createGameObject("brickCube", Vector3(0, -50, 0.0f), Vector3(1, 1, 1));
+	brickCube->AddComponent(m_graphics->getResourceDevice()->createModel("Models/cube/", "Cube.obj", GfxShader::DEFAULT));
+	brickCube->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(100, 2, 100)));
+
+	Ref<GameObject> go1 = createGameObject("object1", Vector3(0, 0, 20.0f));
 	go1->AddComponent(m_graphics->getResourceDevice()->createModel("Models/nanosuit/", "nanosuit.obj", GfxShader::DEFAULT));
 }
