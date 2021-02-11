e5564dd0bd884a29ad5989dc73fd4706bd62670c
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-11 17:49:54 +0100

    Fix broken capsuleCollider

--- Season_Shift/Season_Shift/CapsuleCollider.cpp
+++ Season_Shift/Season_Shift/CapsuleCollider.cpp
@@ -42,6 +42,19 @@ void CapsuleCollider::update()
     m_capsule.PointA = m_transform->getPosition() + m_length * unitY;
 }
 
+Vector3 closestPointOnSegmentFromPoint(Vector3 segmentEndPoint1, Vector3 segmentEndPoint2, Vector3 point)
+{
+    //closest point on linesegment to point
+    //från boken Real time collision detection
+
+    Vector3 ab = segmentEndPoint1 - segmentEndPoint2;
+    float t = ab.Dot(point - segmentEndPoint2) / ab.Dot(ab);
+
+    if (t < 0.0f) t = 0.0f;
+    if (t > 1.0f) t = 1.0f;
+
+    return segmentEndPoint2 + t * ab;
+}
 
 bool CapsuleCollider::collide(const Ref<Collider>& collider)
 {
@@ -50,24 +63,6 @@ bool CapsuleCollider::collide(const Ref<Collider>& collider)
 
 
     Ref<OrientedBoxCollider> obb = std::dynamic_pointer_cast<OrientedBoxCollider>(collider);
-
-    //Vector3 normalA;
-    //Vector3 pointA = obb->closestPointOnObb(m_capsule.PointA, normalA);
-
-    //Vector3 normalB;
-    //Vector3 pointB = obb->closestPointOnObb(m_capsule.PointB, normalB);
-
-    //if (normalA == normalB) // capsule can't have collided with two faces
-    //{
-    //    if ((m_capsule.PointA - pointA).Length() < m_capsule.radius || (m_capsule.PointB - pointB).Length() < m_capsule.radius)
-    //    {
-    //        return true;   
-    //    }
-    //}
-    
-
-
-    //--------------------------------
 
     //unit vectors from obb
     Vector3 obbUnitX;
@@ -92,90 +87,109 @@ bool CapsuleCollider::collide(const Ref<Collider>& collider)
     Vector3 capsuleMiddlePoint = (m_capsule.PointA + m_capsule.PointB) / 2;
     Vector3 obbToCapsule = capsuleMiddlePoint - obb->getInternalCollider().Center;
 
-    Vector3 planeNormal;
-    Vector3 planePoint;
-    float dot = 0;
-    float tempDot = 0;
-    tempDot = obbToCapsule.Dot(obbUnitX);
-    if (tempDot > dot)
+    Vector3 planeNormal1;
+    Vector3 planeNormal2;
+    Vector3 planeNormal3;
+    Vector3 planePoint1;
+    Vector3 planePoint2;
+    Vector3 planePoint3;
+
+    if (obbToCapsule.Dot(obbUnitX) > 0)
     {
-        dot = tempDot;
-        planeNormal = obbUnitX;
-        planePoint = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.x * planeNormal;
+        planeNormal1 = obbUnitX;
+        planePoint1 = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.x * planeNormal1;
     }
-    tempDot = obbToCapsule.Dot(-obbUnitX);
-    if (tempDot > dot)
+    else
     {
-        dot = tempDot;
-        planeNormal = -obbUnitX;
-        planePoint = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.x * planeNormal;
+        planeNormal1 = -obbUnitX;
+        planePoint1 = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.x * planeNormal1;
     }
-    tempDot = obbToCapsule.Dot(obbUnitY);
-    if (tempDot > dot)
+    if (obbToCapsule.Dot(obbUnitY) > 0)
     {
-        dot = tempDot;
-        planeNormal = obbUnitY;
-        planePoint = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.y * planeNormal;
+        planeNormal2 = obbUnitY;
+        planePoint2 = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.y * planeNormal2;
     }
-    tempDot = obbToCapsule.Dot(-obbUnitY);
-    if (tempDot > dot)
+    else
     {
-        dot = tempDot;
-        planeNormal = -obbUnitY;
-        planePoint = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.y * planeNormal;
+        planeNormal2 = -obbUnitY;
+        planePoint2 = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.y * planeNormal2;
     }
-    tempDot = obbToCapsule.Dot(obbUnitZ);
-    if (tempDot > dot)
+    if (obbToCapsule.Dot(obbUnitZ) > 0)
     {
-        dot = tempDot;
-        planeNormal = obbUnitZ;
-        planePoint = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.z * planeNormal;
+        planeNormal3 = obbUnitZ;
+        planePoint3 = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.z * planeNormal3;
     }
-    tempDot = obbToCapsule.Dot(-obbUnitZ);
-    if (tempDot > dot)
+    else
     {
-        dot = tempDot;
-        planeNormal = -obbUnitZ;
-        planePoint = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.z * planeNormal;
+        planeNormal3 = -obbUnitZ;
+        planePoint3 = obb->getInternalCollider().Center + obb->getInternalCollider().Extents.z * planeNormal3;
     }
     
     /*Logger::getLogger().debugLog(std::to_string(planeNormal.x) +
         ", " + std::to_string(planeNormal.y) +
         ", " + std::to_string(planeNormal.z) + "\n");*/
 
-    DirectX::XMVECTOR closestPlane = DirectX::XMPlaneFromPointNormal(planePoint, planeNormal);
+    DirectX::XMVECTOR closestPlane1 = DirectX::XMPlaneFromPointNormal(planePoint1, planeNormal1);
+    DirectX::XMVECTOR closestPlane2 = DirectX::XMPlaneFromPointNormal(planePoint2, planeNormal2);
+    DirectX::XMVECTOR closestPlane3 = DirectX::XMPlaneFromPointNormal(planePoint3, planeNormal3);
 
     //find point on plane, OBS! this does not always give "right" result
 
-    DirectX::XMVECTOR planeIntersectionPoint = DirectX::XMPlaneIntersectLine(closestPlane, m_capsule.PointA, m_capsule.PointB);
+    DirectX::XMVECTOR planeIntersectionPoint1 = DirectX::XMPlaneIntersectLine(closestPlane1, m_capsule.PointA, m_capsule.PointB);
+    DirectX::XMVECTOR planeIntersectionPoint2 = DirectX::XMPlaneIntersectLine(closestPlane2, m_capsule.PointA, m_capsule.PointB);
+    DirectX::XMVECTOR planeIntersectionPoint3 = DirectX::XMPlaneIntersectLine(closestPlane3, m_capsule.PointA, m_capsule.PointB);
 
 
 
     //closeset point on plane to obb, OBS! 
     Vector3 trash;
-    Vector3 closestPoint = obb->closestPointOnObb(planeIntersectionPoint, trash);
+    Vector3 closestPoint1 = obb->closestPointOnObb(planeIntersectionPoint1, trash);
+    Vector3 closestPoint2 = obb->closestPointOnObb(planeIntersectionPoint2, trash);
+    Vector3 closestPoint3 = obb->closestPointOnObb(planeIntersectionPoint3, trash);
 
 
     //closest point on linesegment to point
-    //från boken Real time collision detection
+        
+
+    closestPoint1 = closestPointOnSegmentFromPoint(m_capsule.PointB, m_capsule.PointA, closestPoint1);
+    closestPoint2 = closestPointOnSegmentFromPoint(m_capsule.PointB, m_capsule.PointA, closestPoint2);
+    closestPoint3 = closestPointOnSegmentFromPoint(m_capsule.PointB, m_capsule.PointA, closestPoint3);
+
+    Vector3 collisionNormal1;
+    Vector3 collisionNormal2;
+    Vector3 collisionNormal3;
+
+    Vector3 closestPointOnObb1 = obb->closestPointOnObb(closestPoint1, collisionNormal1);
+    Vector3 closestPointOnObb2 = obb->closestPointOnObb(closestPoint2, collisionNormal2);
+    Vector3 closestPointOnObb3 = obb->closestPointOnObb(closestPoint3, collisionNormal3);
     
-    Vector3 ab = m_capsule.PointB - m_capsule.PointA;
-    float t = ab.Dot(closestPoint - m_capsule.PointA) / ab.Dot(ab);
-
-    if (t < 0.0f) t = 0.0f;
-    if (t > 1.0f) t = 1.0f;
-
-    closestPoint = m_capsule.PointA + t * ab;
-
-    Vector3 collisionNormal;
-    Vector3 closestPointOnObb = obb->closestPointOnObb(closestPoint, collisionNormal);
-    if ((closestPoint - closestPointOnObb).Length() <= m_capsule.radius)
+    float pen = 0;
+    if (m_capsule.radius - (closestPoint1 - closestPointOnObb2).Length() > pen)
     {
-        m_collisionInfo.m_collisionPoint = closestPointOnObb;
-        m_collisionInfo.m_segmentPoint = closestPoint;
-        m_collisionInfo.m_normal = collisionNormal;
-        m_collisionInfo.m_penetration = m_capsule.radius - (closestPoint - closestPointOnObb).Length();
-
+        m_collisionInfo.m_collisionPoint = closestPointOnObb1;
+        m_collisionInfo.m_segmentPoint = closestPoint1;
+        m_collisionInfo.m_normal = collisionNormal1;
+        m_collisionInfo.m_penetration = m_capsule.radius - (closestPoint1 - closestPointOnObb1).Length();
+        pen = m_capsule.radius - (closestPoint1 - closestPointOnObb1).Length();
+    }
+    if (m_capsule.radius - (closestPoint2 - closestPointOnObb2).Length() > pen)
+    {
+        m_collisionInfo.m_collisionPoint = closestPointOnObb2;
+        m_collisionInfo.m_segmentPoint = closestPoint2;
+        m_collisionInfo.m_normal = collisionNormal2;
+        m_collisionInfo.m_penetration = m_capsule.radius - (closestPoint2 - closestPointOnObb2).Length();
+        pen = m_capsule.radius - (closestPoint2 - closestPointOnObb2).Length();
+    }
+    if (m_capsule.radius - (closestPoint3 - closestPointOnObb3).Length() > pen)
+    {
+        m_collisionInfo.m_collisionPoint = closestPointOnObb3;
+        m_collisionInfo.m_segmentPoint = closestPoint3;
+        m_collisionInfo.m_normal = collisionNormal3;
+        m_collisionInfo.m_penetration = m_capsule.radius - (closestPoint3 - closestPointOnObb3).Length();
+        pen = m_capsule.radius - (closestPoint3 - closestPointOnObb3).Length();
+    }
+    if (pen > 0)
+    {
         return true;
     }
     else
@@ -183,6 +197,8 @@ bool CapsuleCollider::collide(const Ref<Collider>& collider)
         return false;
     }
 }
+
+
 
 
 const Capsule& CapsuleCollider::getInternalCollider()
