bf131c8a156237d1a704140949dc5010635796e1
Jumpscaren <58552272+Jumpscaren@users.noreply.github.com>
2021-02-12 14:51:58 +0100

    Fix jump

--- Season_Shift/Season_Shift/Scenes/Scene3.cpp
+++ Season_Shift/Season_Shift/Scenes/Scene3.cpp
@@ -30,15 +30,15 @@ void Scene3::setUpScene()
 	brickCube->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
 	brickCube->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(400, 2, 400)));
 
-	Ref<GameObject> cube = createGameObject("brickCube", Vector3(0, 5.0f, 0.0f), Vector3(1.0f / 25.0f, 1, 1.0f / 25.0f));
+	Ref<GameObject> cube = createGameObject("brickCube", Vector3(0, 5.0f, 0.0f), Vector3(1.0f / 10.0f, 1, 1.0f / 10.0f));
 	cube->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
-	cube->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(8, 2, 8)));
+	cube->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20, 2, 20)));
 
-	Ref<GameObject> cube2 = createGameObject("brickCube", Vector3(30.0f, 10.0f, 0.0f), Vector3(1.0f / 25.0f, 1, 1.0f / 25.0f));
+	Ref<GameObject> cube2 = createGameObject("brickCube", Vector3(30.0f, 10.0f, 0.0f), Vector3(1.0f / 10.0f, 1, 1.0f / 10.0f));
 	cube2->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
-	cube2->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(8, 2, 8)));
+	cube2->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20, 2, 20)));
 
-	Ref<GameObject> cube3 = createGameObject("brickCube", Vector3(80.0f, 15.0f, 0.0f), Vector3(1.0f / 25.0f, 1, 1.0f / 25.0f));
+	Ref<GameObject> cube3 = createGameObject("brickCube", Vector3(80.0f, 15.0f, 0.0f), Vector3(1.0f / 10.0f, 1, 1.0f / 10.0f));
 	cube3->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
-	cube3->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(8, 2, 8)));
+	cube3->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20, 2, 20)));
 }
