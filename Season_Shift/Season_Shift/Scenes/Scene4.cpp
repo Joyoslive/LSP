efaa4a2c63af1d7ba21ae05c75f267ec1f5deb10
SamilamX <30898665+SamilamX@users.noreply.github.com>
2021-02-16 15:41:12 +0100

    Update Scene4.cpp

--- Season_Shift/Season_Shift/Scenes/Scene4.cpp
+++ Season_Shift/Season_Shift/Scenes/Scene4.cpp
@@ -32,9 +32,9 @@ void Scene4::setUpScene()
 	playerJumpTrigger->AddComponent(std::make_shared<SphereCollider>(1));
 	playerJumpTrigger->AddComponent(std::make_shared<PlayerJumpTrigger>(player));
 
-	Ref<GameObject> brickCube = createGameObject("brickCube", Vector3(0, -50, 0.0f), Vector3(2 * 5, 1, 2 * 5));
+	Ref<GameObject> brickCube = createGameObject("brickCube", Vector3(0, -50, 0.0f), Vector3(2 * 50, 1, 2 * 50));
 	brickCube->AddComponent(m_graphics->getResourceDevice()->createModel("Models/ground/", "200x2x200Box.obj", GfxShader::DEFAULT));
-	brickCube->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(400 * 5, 2, 400 * 5)));
+	brickCube->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(400 * 50, 2, 400 * 50)));
 
 	Ref<GameObject> start = createGameObject("brickCube", Vector3(0, 5.0f, 0.0f), Vector3(1.0f / 10.0f, 1, 1.0f / 10.0f));
 	start->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
@@ -61,15 +61,30 @@ void Scene4::setUpScene()
 	springBox4->AddComponent(m_graphics->getResourceDevice()->createModel("Models/boxSpring/", "200x2x200Box.obj", GfxShader::DEFAULT));
 	springBox4->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 7.5, 2 * 7.5, 20 * 1.2)));
 
-	Ref<GameObject> springBox6 = createGameObject("brickCube", Vector3(10.0f + (20 * 60) / 2, 45.0f, 70.0f), Vector3((1.0f / 10.0f) * 5, 1 * 4.5, (1.0f / 10.0f) * 1.5));
-	springBox5->AddComponent(m_graphics->getResourceDevice()->createModel("Models/boxSpring/", "200x2x200Box.obj", GfxShader::DEFAULT));
-	springBox5->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 5, 2 * 4.5, 20 * 1.5)));
+	Ref<GameObject> springBox6 = createGameObject("brickCube", Vector3(10.0f + (20 * 125) / 2, 45.0f, 70.0f), Vector3((1.0f / 10.0f) * 4, 1 * 3.5, (1.0f / 10.0f) * 1.25));
+	springBox6->AddComponent(m_graphics->getResourceDevice()->createModel("Models/boxSpring/", "200x2x200Box.obj", GfxShader::DEFAULT));
+	springBox6->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 4, 2 * 3.5, 20 * 1.25)));
 
-	Ref<GameObject> springBox7 = createGameObject("brickCube", Vector3(10.0f + (20 * 60) / 2, 45.0f, 50.0f), Vector3((1.0f / 10.0f) * 5, 1 * 4.5, (1.0f / 10.0f) * 1.5));
-	springBox5->AddComponent(m_graphics->getResourceDevice()->createModel("Models/boxSpring/", "200x2x200Box.obj", GfxShader::DEFAULT));
-	springBox5->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 5, 2 * 4.5, 20 * 1.5)));
+	Ref<GameObject> springBox7 = createGameObject("brickCube", Vector3(10.0f + (20 * 155) / 2, 25.0f, 90.0f), Vector3((1.0f / 10.0f) * 3, 1 * 2.5, (1.0f / 10.0f) * 1));
+	springBox7->AddComponent(m_graphics->getResourceDevice()->createModel("Models/boxSpring/", "200x2x200Box.obj", GfxShader::DEFAULT));
+	springBox7->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 3, 2 * 2.5, 20 * 1)));
+
+	Ref<GameObject> springBoxTower = createGameObject("brickCube", Vector3(10.0f + (20 * 200) / 2, 50.0f, 90.0f), Vector3((1.0f / 10.0f) * 2, 1 * 200, (1.0f / 10.0f) * 2));
+	springBoxTower->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
+	springBoxTower->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 2, 2 * 200, 20 * 2)));
+
+	Ref<GameObject> springBoxTowerPlatform1 = createGameObject("brickCube", Vector3(10.0f + (20 * 190) / 2, 50.0f, 90.0f), Vector3((1.0f / 10.0f) * 2, 1 * 2, (1.0f / 10.0f) * 2));
+	springBoxTowerPlatform1->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
+	springBoxTowerPlatform1->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 2, 2 * 2, 20 * 2)));
+
+	Ref<GameObject> springBoxTowerPlatform2 = createGameObject("brickCube", Vector3(10.0f + (20 * 200) / 2, 70.0f, 150.0f), Vector3((1.0f / 10.0f) * 2, 1 * 2, (1.0f / 10.0f) * 2));
+	springBoxTowerPlatform2->AddComponent(m_graphics->getResourceDevice()->createModel("Models/box/", "200x2x200Box.obj", GfxShader::DEFAULT));
+	springBoxTowerPlatform2->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(20 * 2, 2 * 2, 20 * 2)));
 
 
+	Ref<GameObject> go1 = createGameObject("goal", Vector3(10.0f + (20 * 180) / 2, 280.0f, 90.0f));
+	go1->AddComponent(m_graphics->getResourceDevice()->createModel("Models/nanosuit/", "nanosuit.obj", GfxShader::DEFAULT));
+	go1->AddComponent(std::make_shared<OrientedBoxCollider>(Vector3(10 * 2, 2 * 2, 10 * 2)));
 	//Winter
 	Ref<GameObject> winterBox1 = createGameObject("brickCube", Vector3(0.0, 5.0f, 10.0f+(20*2)/2), Vector3((1.0f / 10.0f), 1, (1.0f / 10.0f)*2));
 	winterBox1->AddComponent(m_graphics->getResourceDevice()->createModel("Models/boxWinter/", "200x2x200Box.obj", GfxShader::DEFAULT));
