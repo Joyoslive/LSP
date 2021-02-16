3ade831851100f7a16162dacfcfe6eb051fd93dd
Jumpscaren <58552272+Jumpscaren@users.noreply.github.com>
2021-02-16 20:06:35 +0100

    change scene

--- Season_Shift/Season_Shift/Source.cpp
+++ Season_Shift/Season_Shift/Source.cpp
@@ -30,7 +30,7 @@ int WINAPI wWinMain(_In_ HINSTANCE inst, _In_opt_ HINSTANCE prevInst, _In_ LPWST
 
 	SceneManager sceneManager = SceneManager(&gph);
 
-	sceneManager.changeScene(3);
+	sceneManager.changeScene(2);
 	Ref<Scene> scene = sceneManager.getActiveScene();
 
 	Ref<GameObject> player = scene->getGameObject("player");
