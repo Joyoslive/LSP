3cefca824fd882497e57e55f9b3fca742e78e1c0
Joyos <joyoslive@gmail.com>
2021-02-17 10:38:48 +0100

    Add scene5 as a test scene for the scene reader

---
+++ Season_Shift/Season_Shift/Scene5.cpp
@@ -0,0 +1,10 @@
+#include "Scene5.h"
+
+Scene5::Scene5(Graphics* graphics) : Scene(graphics)
+{}
+
+void Scene5::setUpScene()
+{
+	// Scene reader call to read a json file to scene
+	// Post setup, like cameras and logic
+}
