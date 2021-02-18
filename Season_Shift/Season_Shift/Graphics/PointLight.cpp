9805d3a3dcd71fd2a2dd684080053a5943f5754c
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-18 10:01:08 +0100

    Add seters in PointLight and remove unique id check

--- Season_Shift/Season_Shift/Graphics/PointLight.cpp
+++ Season_Shift/Season_Shift/Graphics/PointLight.cpp
@@ -4,21 +4,21 @@
 using namespace DirectX::SimpleMath;
 
 int PointLight::s_nameNumber = 0;
-std::vector<std::string> PointLight::s_names = std::vector<std::string>();
+//std::vector<std::string> PointLight::s_names = std::vector<std::string>();
 
 void PointLight::generateName()
 {
-	bool unique = true;
+	//bool unique = true;
 	this->m_name = "pointLight" + std::to_string(s_nameNumber++);
 
-	for (auto& str : s_names)
-	{
-		if (str == m_name)
-		{
-			unique = false;
-		}
-	}
-	if(!unique) generateName();
+	//for (auto& str : s_names)
+	//{
+	//	if (str == m_name)
+	//	{
+	//		unique = false;
+	//	}
+	//}
+	//if(!unique) generateName();
 }
 
 PointLight::PointLight(Vector3 position, Vector3 color, float strength, std::string name) : PointLight(position, name)
@@ -32,16 +32,52 @@ PointLight::PointLight(Vector3 position, std::string name)
 	m_color = Vector3(1, 1, 1);
 	m_strength = 1.0f;
 	m_position = position;
+	m_lit = true;
 	m_name = name;
 
 	if (name == "")
 	{
 		generateName();
 	}
-	for (auto& str : s_names)
+	/*for (auto& str : s_names)
 	{
 		assert(str != m_name);
 	}
 	
-	s_names.push_back(m_name);
+	s_names.push_back(m_name);*/
+}
+
+void PointLight::setColor(float r, float g, float b)
+{	
+	m_color = Vector3(r, g, b);
+}
+
+void PointLight::setColor(const DirectX::SimpleMath::Vector3& rgb)
+{
+	m_color = rgb;
+}
+
+void PointLight::setColor(unsigned char r, unsigned char g, unsigned char b)
+{
+	m_color = Vector3(r / 255.0f, g / 255.0f, b / 255.0f);
+}
+
+void PointLight::setPosition(const DirectX::SimpleMath::Vector3& position)
+{
+	m_position = position;
+}
+
+void PointLight::setPosition(float x, float y, float z)
+{
+	m_position = Vector3(x, y, z);
+}
+
+void PointLight::setStrength(float strength)
+{
+	m_strength = strength;
+}
+
+void PointLight::setOnOff(bool on)
+{
+	m_lit = on;
 }
--- Season_Shift/Season_Shift/Graphics/PointLight.h
+++ Season_Shift/Season_Shift/Graphics/PointLight.h
@@ -11,9 +11,10 @@ private:
 	DirectX::SimpleMath::Vector3 m_position;
 	DirectX::SimpleMath::Vector3 m_color;
 	float m_strength;
+	bool m_lit;
 	std::string m_name;
 
-	static std::vector<std::string> s_names;
+	//static std::vector<std::string> s_names;
 	static int s_nameNumber;
 
 private:
@@ -22,5 +23,17 @@ public:
 
 	PointLight(DirectX::SimpleMath::Vector3 position, DirectX::SimpleMath::Vector3 color, float strength, std::string name = "");
 	PointLight(DirectX::SimpleMath::Vector3 position, std::string name = "");
+	~PointLight() = default;
+
+	void setColor(float r, float g, float b);
+	void setColor(const DirectX::SimpleMath::Vector3& rgb);
+	void setColor(unsigned char r, unsigned char g, unsigned char b);
+
+	void setPosition(const DirectX::SimpleMath::Vector3& position);
+	void setPosition(float x, float y, float z);
+
+	void setStrength(float strength);
+
+	void setOnOff(bool on);
 };
 
