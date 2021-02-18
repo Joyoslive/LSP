d2cca4e7241f67351f9ebd2b63722bb409422444
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-18 14:19:36 +0100

    Add most of what should be needed for pointLights on cpu side

--- Season_Shift/Season_Shift/Graphics/PointLightRepo.h
+++ Season_Shift/Season_Shift/Graphics/PointLightRepo.h
@@ -1,5 +1,50 @@
 #pragma once
-class PointLightRepo
+#include "PointLight.h"
+#include "DX/DXBuffer.h"
+#include "DX\DXDevice.h"
+#include <vector>
+#include <memory>
+#include <utility>
+#include <string>
+
+
+class PointLightRepo : public std::enable_shared_from_this<PointLightRepo>
 {
+	friend PointLight;
+private:
+
+
+	bool m_rebuildBuffer;
+	size_t m_latestUsedLightIndex;
+	D3D11_SUBRESOURCE_DATA m_subresData;
+
+	std::shared_ptr<DXDevice> m_dxDev;
+
+	std::shared_ptr<DXBuffer> m_structuredBuffer;
+
+
+	std::vector<std::pair<PointLight, int>> m_pointLightVector;
+	std::vector<PointLight::PointLightResource> m_pointLightResourceVector;
+
+public:
+	PointLightRepo(std::shared_ptr<DXDevice> dxDev);
+	~PointLightRepo();
+
+
+	void addPointLight(PointLight& pointLight);
+
+	
+	
+
+	bool findPointLight(const std::string& name);
+	const PointLight& getPointLight(const std::string& name);
+
+	std::shared_ptr<DXBuffer> getStructuredLightBuffer();
+
+
+private:
+	void createBuffer();
+	void replacePointLightResource(PointLight::PointLightResource data, int index);
+	void turnOnOff(bool on, int index);
 };
 
