e04c0c06d3562dafa641595dd06654b8e8a9b47c
Joyos <joyoslive@gmail.com>
2021-02-18 13:36:26 +0100

    Make scene5 read itself from scene.json

--- Season_Shift/Season_Shift/Scenes/Scene5.cpp
+++ Season_Shift/Season_Shift/Scenes/Scene5.cpp
@@ -12,10 +12,19 @@ Scene5::Scene5(Graphics* graphics) : Scene(graphics)
 void Scene5::setUpScene()
 {
 	SceneReader sr;
-	auto vec = sr.readSceneFromFile("file");
+	auto vec = sr.readSceneFromFile("Scenes/SceneJSONs/scene.json");
+
+	for (auto& object : vec)
+	{
+		auto go = createGameObject(object.name, object.position, object.scale/2, object.rotation);
+		auto model = m_graphics->getResourceDevice()->createModel("Models/Cube/", "Cube.obj", GfxShader::DEFAULT);
+		auto collider = std::make_shared<OrientedBoxCollider>(object.scale);
+		go->AddComponent(model);
+		go->AddComponent(collider);
+	}
 	
 	// Post setup, like cameras and logic
-	auto player = createGameObject("player");
+	auto player = createGameObject("player", Vector3(0, 10, 0));
 	player->AddComponent(std::make_shared<CameraComponent>());
 	player->AddComponent(std::make_shared<RigidBody>());
 	player->AddComponent(std::make_shared<Player>());
