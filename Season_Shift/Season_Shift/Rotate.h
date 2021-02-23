f6af4ce2dd5855fa2cb0aa4f8b1976a4cf0016f0
SamilamX <30898665+SamilamX@users.noreply.github.com>
2021-02-23 16:06:56 +0100

    rotating platforms

--- Season_Shift/Season_Shift/Rotate.h
+++ Season_Shift/Season_Shift/Rotate.h
@@ -6,8 +6,10 @@ class Rotate : public Logic
 {
 private:
 	DirectX::SimpleMath::Vector3 rot;
+	float m_x, m_y, m_z;
 public:
 	Rotate();
+	Rotate(float x, float y, float z);
 	~Rotate();
 	void update() override;
 };
