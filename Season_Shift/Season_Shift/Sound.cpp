028e0289d5d75f099a8db914fefe0e42f96cf9ad
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-23 15:54:53 +0100

    Add Exploding sound to player that does not stop

--- Season_Shift/Season_Shift/Sound.cpp
+++ Season_Shift/Season_Shift/Sound.cpp
@@ -1,11 +1,21 @@
 #include "Sound.h"
 
+float explodeDelay = 30.0f;
+
 void Sound::update()
 {
+	explodeDelay -= 0.01; //dt??
+	if (explodeDelay < 0.f)
+	{
+		m_sound->Play();
+		explodeDelay = 5.0;
+	}
 }
 
 Sound::Sound()
 {
+	m_audioEngine = DirectX::AudioEngine();
+	m_sound = std::make_shared<DirectX::SoundEffect>(&m_audioEngine, L"Sounds/Explo4.wav");
 	m_componentType = ComponentEnum::SOUND;
 }
 
