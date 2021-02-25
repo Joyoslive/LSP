7f515fd0ed178b99854dbc6c359da47fb72e8d3a
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-25 17:29:39 +0100

    Add 3d audio,  does not update before the next sound => ganska trash

--- Season_Shift/Season_Shift/Sound.cpp
+++ Season_Shift/Season_Shift/Sound.cpp
@@ -1,34 +1,92 @@
 #include "pch.h"
 #include "Sound.h"
+#include "Transform.h"
+#include "CameraComponent.h"
 
 
 using namespace std;
+HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
-void Sound::update()
-{
-}
 
-Sound::Sound(const vector<string>& fileNames)
+Sound::Sound(const vector<string>& fileNames, const Ref<CameraComponent>& listener)
 {
-	m_audioEngine = DirectX::AudioEngine();
 	m_componentType = ComponentEnum::SOUND;
+	m_listenerCamera = listener;
+	if (listener != nullptr)
+	{
+		m_audioEngine = std::make_unique<DirectX::AudioEngine>(DirectX::AudioEngine_EnvironmentalReverb 
+			| DirectX::AudioEngine_ReverbUseFilters
+			| DirectX::AudioEngine_UseMasteringLimiter
+			| DirectX::AudioEngine_DisableVoiceReuse); //this may work,
+		m_emitter = DirectX::AudioEmitter();
+		m_listener = DirectX::AudioListener();
+	}
+	else
+	{
+		m_audioEngine = std::make_unique<DirectX::AudioEngine>();
+	}
 
 	std::wstring ws;
 	for (int i = 0; i < fileNames.size(); ++i)
 	{
 		ws = std::wstring(fileNames[i].begin(), fileNames[i].end());
-		m_sounds.emplace_back(&m_audioEngine, ws.c_str());
+		m_sounds.emplace_back(m_audioEngine.get(), ws.c_str());
 		m_map[fileNames[i]] = i;
 	}
 }
 
 Sound::~Sound()
 {
-	m_audioEngine.Suspend();
+	if (m_effectInst != nullptr) m_effectInst.reset();
+	m_audioEngine->Suspend();
 }
+
+void Sound::update()
+{
+	m_audioEngine->Update();
+	if (m_listenerCamera != nullptr)
+	{
+		m_emitter.SetPosition(getTransform()->getPosition());
+		m_emitter.SetOrientation(getTransform()->getWorldMatrix().Forward(), getTransform()->getWorldMatrix().Up());
+
+		m_listener.SetPosition(m_listenerCamera->getCamera()->getPosition());
+		m_listener.SetOrientation(m_listenerCamera->getCamera()->getForward(), m_listenerCamera->getCamera()->getUp());
+	}
+}
+
+void Sound::initialize()
+{
+	if (m_listenerCamera != nullptr)
+	{
+		m_emitter.SetPosition(getTransform()->getPosition());
+		m_emitter.SetOrientation(getTransform()->getWorldMatrix().Forward(), getTransform()->getWorldMatrix().Up());
+
+		m_listener.SetPosition(m_listenerCamera->getCamera()->getPosition());
+		m_listener.SetOrientation(m_listenerCamera->getCamera()->getForward(), m_listenerCamera->getCamera()->getUp());
+	}
+}
+
+
 
 size_t Sound::play(const std::string& soundName)
 {
-	m_sounds[m_map[soundName]].Play();
+	if (m_listenerCamera != nullptr)
+	{
+		m_effectInst = m_sounds[m_map[soundName]].CreateInstance(DirectX::SoundEffectInstance_Use3D);
+		m_effectInst->Play(true);
+		m_effectInst->Apply3D(m_listener, m_emitter, false);
+	}
+	else
+	{
+		m_effectInst = m_sounds[m_map[soundName]].CreateInstance();
+		m_effectInst->Play();
+	}
 	return m_sounds[m_map[soundName]].GetSampleDuration();
+
+
+}
+
+void Sound::setVolume(float volume)
+{
+	m_audioEngine->SetMasterVolume(volume);
 }
