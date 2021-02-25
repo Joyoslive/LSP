333d390f0b4e3405c2d11e295b54eaf7ceb32f60
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-02-25 12:56:13 +0100

    Add Constructor to Sound

--- Season_Shift/Season_Shift/Sound.cpp
+++ Season_Shift/Season_Shift/Sound.cpp
@@ -2,15 +2,24 @@
 #include "Sound.h"
 
 
+using namespace std;
+
 void Sound::update()
 {
 }
 
-Sound::Sound()
+Sound::Sound(vector<string>& fileNames)
 {
 	m_audioEngine = DirectX::AudioEngine();
-	m_sound = std::make_shared<DirectX::SoundEffect>(&m_audioEngine, L"Sounds/Explo4.wav");
 	m_componentType = ComponentEnum::SOUND;
+
+	std::wstring ws;
+	for (int i = 0; i < fileNames.size(); ++i)
+	{
+		ws = std::wstring(fileNames[i].begin(), fileNames[i].end());
+		m_sounds.emplace_back(&m_audioEngine, ws.c_str());
+		m_map[fileNames[i]] = i;
+	}
 }
 
 Sound::~Sound()
@@ -20,6 +29,6 @@ Sound::~Sound()
 
 size_t Sound::play()
 {
-	m_sound->Play();
-	return m_sound->GetSampleDuration();
+	m_sounds[0].Play();
+	return m_sounds[0].GetSampleDuration();
 }
