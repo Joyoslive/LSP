0a5c380c1e89f09a3c7841622e297bce4bcd751a
Joyos <joyoslive@gmail.com>
2021-03-01 13:34:52 +0100

    Change setText to not be pure virtual
    So it doesn't have to be implemented in every ISprite descendant

--- Season_Shift/Season_Shift/Graphics/2D/ISprite.h
+++ Season_Shift/Season_Shift/Graphics/2D/ISprite.h
@@ -7,7 +7,7 @@ class ISprite
 public:
 	virtual void draw(const std::shared_ptr<DirectX::SpriteBatch>& spriteBatch) = 0;
 	// Only applicable to Font (probably, but we want his exposed)
-	virtual void setText(const std::string& text) = 0;
+	virtual void setText(const std::string& text) { };
 	void setPosition(const DirectX::SimpleMath::Vector2 position) { m_position = position; }
 protected:
 	DirectX::SimpleMath::Vector2 m_position;
