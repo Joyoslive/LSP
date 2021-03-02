7d69947569426bb3dec5e9ef086d19d2181c98ad
Joyos <joyoslive@gmail.com>
2021-03-02 09:56:49 +0100

    Change ISprite functions to be explicitly inline

--- Season_Shift/Season_Shift/Graphics/2D/ISprite.h
+++ Season_Shift/Season_Shift/Graphics/2D/ISprite.h
@@ -7,10 +7,10 @@ class ISprite
 public:
 	virtual void draw(const std::shared_ptr<DirectX::SpriteBatch>& spriteBatch) = 0;
 	// Only applicable to Font (probably, but we want this exposed)
-	virtual void setText(const std::string& text) { };
-	virtual void setFont(std::shared_ptr<DirectX::SpriteFont> font) { };
+	virtual inline void setText(const std::string& text) { };
+	virtual inline void setFont(std::shared_ptr<DirectX::SpriteFont> font) { };
 	virtual void setColor(const DirectX::SimpleMath::Color& col) = 0;
-	void setPosition(const DirectX::SimpleMath::Vector2 position) { m_position = position; }
+	inline void setPosition(const DirectX::SimpleMath::Vector2 position) { m_position = position; }
 	
 protected:
 	DirectX::SimpleMath::Vector2 m_position;
