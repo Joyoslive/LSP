57b53f5bf9b3bc2d6a6945f2c07871b1f3b2ed0f
GUNNAR CERNE <bjorngunnar99@gmail.com>
2021-03-09 15:37:07 +0100

    Add random colors

--- Season_Shift/Season_Shift/Graphics/Shaders/ParticleDefaultEmittCS.hlsl
+++ Season_Shift/Season_Shift/Graphics/Shaders/ParticleDefaultEmittCS.hlsl
@@ -41,7 +41,13 @@ static const float3 dir[8] =
     normalize(float3(-1.0f, -1.0f, -1.0f))
 };
 
-
+float3 random(float seed, float3 seedVec)
+{
+    float r1 = frac(sin(dot(float2(seed, seedVec.x), float2(12.9898, 78.233))) * 43758.5453123);
+    float r2 = frac(sin(dot(float2(r1, seedVec.y), float2(12.9898, 78.233))) * 43758.5453123);
+    float r3 = frac(sin(dot(float2(r2, seedVec.z), float2(12.9898, 78.233))) * 43758.5453123);
+    return float3(r1, r2, r3);
+}
 #define size 8
 
 [numthreads(size, 1, 1)]
@@ -57,9 +63,10 @@ void main( uint3 DTid : SV_DispatchThreadID )
         p.pos = pos;
         p.vel = 60 * normalize(reflect(dir[DTid.x], randVec));
         p.scale = scale;
-        p.color = color;
+        //p.color = color
+        p.color = random((float)DTid.x / 8.0f, normalize(randVec));
         p.angle = DTid.x;
-        
+
         appendBuffer.Append(p);
     }
 }
