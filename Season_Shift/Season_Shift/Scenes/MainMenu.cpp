e22eaed727debfd86e26c606c5452d8e14887c6a
Joyos <joyoslive@gmail.com>
2021-03-10 14:26:10 +0100

    Make the main menu buttons be pressed when pressed

--- Season_Shift/Season_Shift/Scenes/MainMenu.cpp
+++ Season_Shift/Season_Shift/Scenes/MainMenu.cpp
@@ -16,14 +16,67 @@ void MainMenu::setUpScene()
 	m_background = resDev->createSpriteTexture("Textures/MainMenu/background.jpg", 0, 0, 2, 2);
 	
 	//Buttons
-	auto dummy = resDev->createSprite("", L"Textures/Sprites/Fonts/font.spritefont", ScreenPos::MIDDLE, ScreenPos::MIDDLE);
-	auto pos = dummy->getPosition();
-	auto playButton = resDev->createSpriteTexture("Textures/MainMenu/Button.png", pos.x - 72.0, pos.y, 4, 1, 0, 0, 
-		[this](){ SwitchScene(Scenes::STAGE_SELECT); });
-	m_buttons.push_back(playButton);
+	m_playText = resDev->createSprite("Play", L"Textures/Sprites/Fonts/font.spritefont", ScreenPos::MIDDLE, ScreenPos::MIDDLE);
+	auto pos = m_playText->getPosition();
+	pos.y += 60;
+	m_playText->setPosition(pos);
+	std::shared_ptr<ISprite> pressedPlayButton = resDev->createSpriteTexture("Textures/MainMenu/ButtonPressed.png", pos.x - 90, pos.y - 14);
+	pressedPlayButton->setShow(false);
+	std::shared_ptr<ISprite> playButton = resDev->createSpriteTexture("Textures/MainMenu/Button.png", pos.x - 90, pos.y - 20);
+	playButton->onClick([this]()
+		{
+			m_buttons[0]->setShow(false);
+			m_buttons[1]->setShow(true);
+			m_playText->setPosition({m_playText->getPosition().x, m_playText->getPosition().y + 6});
+			m_buttons[1]->setClicked(true);
+		});
+	pressedPlayButton->onRelease([this]()
+		{
+			if (m_buttons[0]->getClicked())
+			{
+				m_buttons[0]->setShow(true);
+				m_buttons[1]->setShow(false);
+				SwitchScene(Scenes::STAGE_1);
+			}
+		});
+
+	m_quitText = resDev->createSprite("Quit", L"Textures/Sprites/Fonts/font.spritefont", ScreenPos::MIDDLE, ScreenPos::MIDDLE);
+	pos = m_quitText->getPosition();
+	pos.y += 120;
+	m_quitText->setPosition(pos);
+	std::shared_ptr<ISprite> pressedExitButton = resDev->createSpriteTexture("Textures/MainMenu/ButtonPressed.png", pos.x - 90, pos.y - 14);
+	pressedExitButton->setShow(false);
+	std::shared_ptr<ISprite> exitButton = resDev->createSpriteTexture("Textures/MainMenu/Button.png", pos.x - 90, pos.y - 20);
+	exitButton->onClick([this]()
+		{
+			m_buttons[2]->setShow(false);
+			m_buttons[3]->setShow(true);
+			m_quitText->setPosition({m_quitText->getPosition().x, m_quitText->getPosition().y + 6});
+		});
+	pressedExitButton->onRelease([this]()
+		{
+			if (m_buttons[2]->getClicked())
+			{
+				m_buttons[2]->setShow(true);
+				m_buttons[3]->setShow(false);
+				SwitchScene(Scenes::STAGE_1);
+			}
+		});
 	
+
+	m_buttons.push_back(playButton);
+	m_buttons.push_back(pressedPlayButton);
+	m_buttons.push_back(exitButton);
+	m_buttons.push_back(pressedExitButton);
+
+	// Add to sprite batch - Order matters
 	m_graphics->addToSpriteBatch(m_background);
 	m_graphics->addToSpriteBatch(playButton);
+	m_graphics->addToSpriteBatch(pressedPlayButton);
+	m_graphics->addToSpriteBatch(m_playText);
+	m_graphics->addToSpriteBatch(exitButton);
+	m_graphics->addToSpriteBatch(pressedExitButton);
+	m_graphics->addToSpriteBatch(m_quitText);
 	m_graphics->addToSpriteBatch(m_title);
 
 	m_mainCamera = std::make_shared<Camera>(); // We need a camera in order to display anything
@@ -33,5 +86,4 @@ void MainMenu::SwitchScene(Scenes scene)
 {
 	int idx = (int)scene;
 	m_sceneManager->changeScene(idx);
-	m_graphics->clearSpriteBatch();
 }
--- Season_Shift/Season_Shift/Scenes/MainMenu.h
+++ Season_Shift/Season_Shift/Scenes/MainMenu.h
@@ -10,12 +10,14 @@ class MainMenu : public Scene
 private:
 	enum class Scenes
 	{
-		STAGE_SELECT = 4,
-		STAGE_1 = 1,
-		STAGE_2 = 2
+		STAGE_SELECT = 6,
+		STAGE_1 = 4,
+		STAGE_2 = 5,
 	};
 private:
 	std::shared_ptr<ISprite> m_title;
+	std::shared_ptr<ISprite> m_playText;
+	std::shared_ptr<ISprite> m_quitText;
 	std::vector< std::shared_ptr<ISprite>> m_buttons;
 	std::shared_ptr<ISprite> m_background;
 
