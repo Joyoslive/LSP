039e743106537a1d6f6d6902741bcc3076909d76
Joyos <joyoslive@gmail.com>
2021-03-12 16:06:14 +0100

    Make main menu take you to stage select
    Making the game unplayable :P

--- Season_Shift/Season_Shift/Scenes/StageSelect.cpp
+++ Season_Shift/Season_Shift/Scenes/StageSelect.cpp
@@ -1,17 +1,66 @@
 #include "pch.h"
 #include "StageSelect.h"
+#include "../Input.h"
 
 StageSelect::StageSelect(Graphics* gph, SceneManager* sm) : Scene(gph, sm)
 {}
 
 void StageSelect::setUpScene()
 {
-	auto resDev = m_graphics->getResourceDevice();
-	m_title = resDev->createSprite("Stage Select", L"Textures/Sprites/Fonts/Oleo.spritefont", ScreenPos::MIDDLE, 100.f);
-	m_backButton = resDev->createSpriteTexture("Textures/MainMenu/Button.png", 72.f, 680.f);
-	m_backText = resDev->createSprite("Back", L"Textures/Sprites/Fonts/font.spritefont", 72.f, 680.f);
+	if (m_firstSetup)
+	{
+		m_firstSetup = false;
 
-	m_graphics->addToSpriteBatch(m_title);
-	m_graphics->addToSpriteBatch(m_backButton);
-	m_graphics->addToSpriteBatch(m_backText);
+		auto resDev = m_graphics->getResourceDevice();
+		m_title = resDev->createSprite("Stage Select", L"Textures/Sprites/Fonts/Oleo.spritefont", ScreenPos::MIDDLE, 100.f);
+		m_backButton = resDev->createSpriteTexture("Textures/MainMenu/Button.png", 68.f, 650.f, 0.65);
+		m_backButtonDown = resDev->createSpriteTexture("Textures/MainMenu/ButtonPressed.png", 68.f, 656.f, 0.65);
+		m_backText = resDev->createSprite("Back", L"Textures/Sprites/Fonts/font.spritefont", 127.f, 672.f);
+
+		m_backButtonDown->setShow(false);
+
+		m_backButton->onClick([this]()
+			{
+				m_backButton->setShow(false);
+				m_backButtonDown->setShow(true);
+				m_backText->setPosition({m_backText->getPosition().x, m_backText->getPosition().y + 6});
+			});
+		m_backButtonDown->onRelease([this]()
+			{
+				if (m_backButton->getClicked())
+				{
+					m_backButton->setShow(false);
+					m_backButtonDown->setShow(false);
+					m_backText->setShow(false);
+					m_title->setShow(false);
+
+					m_backText->setPosition({m_backText->getPosition().x, m_backText->getPosition().y - 6});
+
+					Input::getInput().lockMouse(2);
+					m_sceneManager->changeScene(0); // Switch back to main menu
+				}
+			});
+		m_backButtonDown->onGlobalRelease([this]()
+			{
+				if (m_backButton->getClicked())
+				{
+					m_backButton->setShow(true);
+					m_backButtonDown->setShow(false);
+					m_backText->setPosition({m_backText->getPosition().x, m_backText->getPosition().y - 6});
+				}
+			});
+
+		m_graphics->addToSpriteBatch(m_title);
+		m_graphics->addToSpriteBatch(m_backButton);
+		m_graphics->addToSpriteBatch(m_backButtonDown);
+		m_graphics->addToSpriteBatch(m_backText);
+	}
+	else
+	{
+		m_backButton->setShow(true);
+		m_backText->setShow(true);
+		m_title->setShow(true);
+	}
+	
+	m_mainCamera = std::make_shared<Camera>(); // Camera in order to be able to renders
 }
--- Season_Shift/Season_Shift/Scenes/StageSelect.h
+++ Season_Shift/Season_Shift/Scenes/StageSelect.h
@@ -2,15 +2,19 @@
 #include "../Scene.h"
 #include "../Graphics/Graphics.h"
 #include "../Graphics/2D/ISprite.h"
+#include "../SceneManager.h"
 
 class StageSelect : public Scene
 {
 private:
 	std::shared_ptr<ISprite> m_title;
 	std::shared_ptr<ISprite> m_backButton;
+	std::shared_ptr<ISprite> m_backButtonDown;
 	std::shared_ptr<ISprite> m_backText;
 	std::shared_ptr<ISprite> m_stage1Button;
 	std::shared_ptr<ISprite> m_stage2Button;
+
+	bool m_firstSetup = true;
 
 public:
 	StageSelect(Graphics* gph, SceneManager* sm);
