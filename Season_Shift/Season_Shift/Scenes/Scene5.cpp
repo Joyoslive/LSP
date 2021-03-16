3d8c10690fa34eb6b3a0c9e1a2e7801626536d98
Nadhif Ginola <nfginola@gmail.com>
2021-03-16 17:06:35 +0100

    BRIGHTBOX

--- Season_Shift/Season_Shift/Scenes/Scene5.cpp
+++ Season_Shift/Season_Shift/Scenes/Scene5.cpp
@@ -85,7 +85,7 @@ void Scene5::setUpScene()
 		spawnPartSys->AddComponent(std::make_shared<ParticleSystemComponent>(10*5, 5.0f)));
 	spawnPartSysComp->addEmitter(10, 60, 0.1f, Vector3(0.2f, 0.4f, 1));
 
-	m_graphics->loadSkybox("Textures/Skyboxes/EQUISKYBOX");
+	m_graphics->loadSkybox("Textures/Skyboxes/BRIGHTBOX");
 	m_graphics->setSkybox(1);
 	//Ref<GameObject> partSysGo = createGameObject("partSysGo", Vector3(0, 40, 0));
 	//Ref<ParticleSystemComponent> partSys = std::dynamic_pointer_cast<ParticleSystemComponent>(
